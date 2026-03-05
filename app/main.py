995562dd8a9645fb0e90508ed41b3b4f8d697e6a
German Vasquez <vasque93g3r@gmail.com>
2026-03-05 12:19:14 -0600

    fix: commet to trigger the docker image build

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ from app.api.v1.auth import router as auth_router
 from app.api.v1.health import router as health_router
 
 app = FastAPI(title="Auth API")
-
+# comment to trigger the docker image build
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(health_router, prefix="/api/v1")
